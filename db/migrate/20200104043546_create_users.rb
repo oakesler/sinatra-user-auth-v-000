55dc5e1ad049ba7be375e180a9a95946a2cf54e4
Olivia Kesler <oakesler@protonmail.ch>
2020-01-04 04:36:57 +0000

    Automatically backed up by Learn

--- db/migrate/20200104043546_create_users.rb
+++ db/migrate/20200104043546_create_users.rb
@@ -1,5 +1,20 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    
+    create_table :users do |t|
+      t.string :name
+      t.string :email 
+      t.string :password
   end
+end
+
+
+
+class CreateDogs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
+  end
+ 
 end
